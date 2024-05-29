209e8f30dd133e7f2916a81466680ce1d8f78b8b
MiriamPecora <miriampecora97@gmail.com>
2024-05-29 19:43:01 +0200

    first commit

---
+++ vite-hello-world/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import AppTitle from './components/AppTitle.vue'
+import AppHero from './components/AppHero.vue'
+</script>
+
+<template>
+  <AppTitle/>
+  <AppHero/>
+</template>
+
+<style scoped></style>
---
+++ vite-hello-world/src/components/AppHero.vue
@@ -0,0 +1,17 @@
+<script>
+
+</script>
+
+<template>
+<div class="jumbotron">
+    <h2>Jumbo heading</h2>
+    <p>Lorem ipsum dolor sit amet consectetunon. Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto expedita deserunt ipsa ea facere sit aperiam consequuntur unde nisi! Accusantium repudiandae exercitationem odit repellat inventore dolores ipsum perferendis autem corporis. Nam.</p>
+    <hr>
+    <p>More info!</p>
+</div>
+
+</template>
+
+<style scoped>
+
+</style>
